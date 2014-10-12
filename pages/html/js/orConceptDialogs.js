57ae8275e4faf99b5809cd3121c1bf9cf2446901
Horia Constantin <constantin.horia@gmail.com>
2014-10-12 17:13:45 +0200

    added functionality to check that user went through all concept pages added functionality to award points the first time a concept page is read

---
+++ pages/html/js/orConceptDialogs.js
@@ -0,0 +1,68 @@
+var allConcepts = ["or", "testconcept"];
+var CONCEPT_SEPARATOR = "|";
+
+$(document).ready(function(){
+  $('.optimizationModeling').on('click',function() {
+    bootbox.alert($('<div></div>').load( "dialogs/or.html" ), function(result) {
+      awardPointForFirstRead("or");
+      addConceptToReadConcepts("or");
+      checkAllConceptsRead();
+    });
+  });
+});
+
+function awardPointForFirstRead(conceptId){
+  var readConcepts = getReadConceptsArray();;
+  if ($.inArray(conceptId, readConcepts)===-1) {
+    alert("first time reading the concept. giving 10 points");
+  }
+}
+
+function addConceptToReadConcepts(conceptId){
+  var readConcepts = localStorage.getItem("readConcepts"); 
+  if (readConcepts===null){
+    readConcepts = conceptId;
+  } else {
+    readConcepts += CONCEPT_SEPARATOR;
+    readConcepts += conceptId;
+  }
+  localStorage.setItem("readConcepts", readConcepts);
+}
+
+function getReadConceptsArray(){
+  return localStorage.getItem("readConcepts").split(CONCEPT_SEPARATOR);
+}
+
+function checkAllConceptsRead(){
+  var readConcepts = getReadConceptsArray();
+  if (allConcepts.length === intersectStringArrays(readConcepts, allConcepts).length){
+    alert("giving academic badge");
+    //playbasis.rule(playbasisToken, "read", "1", "", function (result) {
+    //  console.log(result);
+    //});
+  }
+}
+
+function intersectStringArrays(array1, array2) {
+   var result = [];
+   // Don't destroy the original arrays
+   var a = array1.slice(0);
+   var b = array2.slice(0);
+   var aLast = a.length - 1;
+   var bLast = b.length - 1;
+   while (aLast >= 0 && bLast >= 0) {
+      if (a[aLast].localeCompare(b[bLast]) > 0 ) {
+         a.pop();
+         aLast--;
+      } else if (a[aLast].localeCompare(b[bLast]) < 0 ){
+         b.pop();
+         bLast--;
+      } else /* they're equal */ {
+         result.push(a.pop());
+         b.pop();
+         aLast--;
+         bLast--;
+      }
+   }
+   return result;
+}
