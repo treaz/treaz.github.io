36ca5794f2941ee651d5df1ebce8f0c57b5d8997
Horia Constantin <constantin.horia@gmail.com>
2014-10-17 21:13:59 +0200

    dashboard working gamification activates based on user

--- pages/html/js/gamificationMechanisms.js
+++ pages/html/js/gamificationMechanisms.js
@@ -1,11 +1,51 @@
 var allInformationPages = [/*13 or concepts*/"operationsResearch", "mathematicalModel", "mathematicalProgramming", "optimizationModeling", "solution", "linearProgramming", "objectiveFunction", "constraints", "feasibleSolution", "parameters", "decisionVariables", "optimalSolution", "nonNegConstraints",/*4 graphical concepts*/"objectiveFunctionContour","optimalSolutionGraphical", "constraintRegionGraphical", "feasibleRegionGraphical"];
 var CONCEPT_SEPARATOR = "|";
 
-function handleConceptGamification(conceptName){
-  awardPointForFirstRead(conceptName);
-      addConceptToReadConcepts(conceptName);
-      checkallInformationPagesRead();
+$(document).ready(function() {
+  var isGamified = localStorage.getItem("isGamified");
+  if (isGamified==="true"){
+    initializeGamificationLibrary();
+    //enalbe playbasis client
+    //append all gamification elements to dom
+    $( "body" ).append( '<div class="pb-userbar" data-pb-displayPoint="exp" ></div>' );
+    $(".nav.navbar-nav").append('<li><a href="dashboard.html">Dashboard</a></li>');
+  } else if (isGamified==="false"){
+    //playbasis is disabled
+  } else {
+    window.close();
+  }
+});
+
+function initializeGamificationLibrary(){
+var playbasis = new Playbasis('2767998996');
+var playbasisToken;
+playbasis.auth('a45ae6e91f0f07125d60f8b92f8948c3', function (result) {
+  playbasisToken=result.response.token;
+});
+
+window.PBAsyncInit = function(){
+      PB.init({
+        api_key:'2767998996',
+        theme_color :'#0e9ce4',
+        playerId :localStorage.getItem("userId")
+      });
+    };(!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://widget.pbapp.net/playbasis/en/all.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','playbasis-js'));
 }
+
+function isGamificationEnabled(){
+  var isGamified = localStorage.getItem("isGamified");
+  return isGamified==="true";
+}
+
+function handleConceptGamification(conceptName){
+  if (isGamificationEnabled()){
+    awardPointForFirstRead(conceptName);
+    addConceptToReadConcepts(conceptName);
+    checkallInformationPagesRead();
+  }
+}
+
+
 
 function awardPointForFirstRead(conceptId){
   var readConcepts = getReadConceptsArray();
--- pages/html/js/signin.js
+++ pages/html/js/signin.js
@@ -1,22 +1,28 @@
-var users = {};
-users["horia"] = "1";
-users["cutKcakcfizR"] = "10";
-users["FVHFOwjwUbqm"] = "11";
-users["ovewzQDfuHod"] = "12";
-users["fXyYxCDpUwFR"] = "13";
-users["uAYrhMIPdDDe"] = "14";
-users["aqdorOGzboop"] = "15";
-users["mOlrTDZcRyxT"] = "16";
-users["uKJYfDjwGgSt"] = "17";
-users["dEywdwCAssIS"] = "18";
-users["zxepkXIBBgPJ"] = "19";
+var normalUsers = ["test","CEaJZYqGIgOw", "dhixTHutsGph", "HpGTPASOBBBb", "ItvyFqBPUkji", "kelGzVwttiSr", "oyOwhrvaIdtm", "YYBLveewYNyq", "NGEkfGvnSPMG", "fpKRBUIdXvSd", "IHXWqdxmdMLU"];
+
+var gamificationUsers = {};
+gamificationUsers["horia"] = "1";
+gamificationUsers["cutKcakcfizR"] = "10";
+gamificationUsers["FVHFOwjwUbqm"] = "11";
+gamificationUsers["ovewzQDfuHod"] = "12";
+gamificationUsers["fXyYxCDpUwFR"] = "13";
+gamificationUsers["uAYrhMIPdDDe"] = "14";
+gamificationUsers["aqdorOGzboop"] = "15";
+gamificationUsers["mOlrTDZcRyxT"] = "16";
+gamificationUsers["uKJYfDjwGgSt"] = "17";
+gamificationUsers["dEywdwCAssIS"] = "18";
+gamificationUsers["zxepkXIBBgPJ"] = "19";
 
 $(document).ready(function(){
-  $(":button").on('click',function() {
-    var username = $(":input").val();
-    if (username in users) {
-      var userId = localStorage.getItem("userId"); 
-      localStorage.setItem("userId", users[username]);
+  $(".buttonLogin").on('click',function() {
+    var username = $(".inputlogin").val();
+    if (username in gamificationUsers) {
+      localStorage.setItem("isGamified", "true");
+      localStorage.setItem("userId", gamificationUsers[username]);
+      window.location.href = "whyOptimizationModeling.html";
+      return false;
+    } else if (normalUsers.indexOf(username) != -1) {
+      localStorage.setItem("isGamified", "false");
       window.location.href = "whyOptimizationModeling.html";
       return false;
     } else {
