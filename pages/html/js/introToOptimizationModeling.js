b960020febd1bc2b0dcf3287d6dd69448daa4384
Horia Constantin <constantin.horia@gmail.com>
2014-12-14 13:22:30 +0100

    corrected eventId for bus and chairs

--- pages/html/js/introToOptimizationModeling.js
+++ pages/html/js/introToOptimizationModeling.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
   $('.chairProblem').on('click',function() {
     bootbox.alert($('<div></div>').load( "modelingLPProblem/chairProblem.html" ), function(result) {
     });
-    pageEvent("chairProblem", "pageOpen");
+    pageEvent("chairProblem", "openPage");
     handleConceptGamification("chairProblem");
   });
   
   $('.busProblem').on('click',function() {
     bootbox.alert($('<div></div>').load( "modelingLPProblem/busProblem.html" ), function(result) {
     });
-    pageEvent("busProblem", "pageOpen");
+    pageEvent("busProblem", "openPage");
     handleConceptGamification("busProblem");
   });
 });
